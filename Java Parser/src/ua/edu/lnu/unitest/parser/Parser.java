d03fc534c2b4e46375980874bd5903422efe89a6
Kostyantyn <kostya@lviv.farlep.net>
2012-05-07 23:46:33 +0300

    encoding

--- Java Parser/src/ua/edu/lnu/unitest/parser/Parser.java
+++ Java Parser/src/ua/edu/lnu/unitest/parser/Parser.java
@@ -1,17 +1,43 @@
 package ua.edu.lnu.unitest.parser;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.regex.*;
 
+/**
+ * Parser for Ohrenda's test
+ * Question must be formatted like this:
+ * <p i>This is a question:
+ * <br>1) this is first answer;
+ * <br>2) this is second answer;
+ * <br>3) this is third answer;
+ * <br>#23
+ * @author KD
+ *
+ */
 public class Parser {
-
+	/**
+	 * Name of input file
+	 */
 	private String inFileName;
+	/**
+	 * Name of output file
+	 */
 	private String outFileName;
-
+	/**
+	 * Var to store input text
+	 */
 	private StringBuilder inTest = new StringBuilder();
+	/**
+	 * Var to store formatted text
+	 */
+	private StringBuilder outTest = new StringBuilder();
 
 	public Parser() {
 
@@ -33,11 +59,17 @@ public class Parser {
 		BufferedReader br = null;
 
 		try {
-			br = new BufferedReader(new FileReader(inFileName));
+//			br = new BufferedReader(new FileReader(inFileName));
+			
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(inFileName), "UTF-16"));
+			
 		} catch (FileNotFoundException e) {
 			System.out.println("File \"" + this.inFileName
 					+ "\" doesn't exist ");
 			return false;
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		String str = null;
@@ -46,8 +78,11 @@ public class Parser {
 				str = br.readLine();
 				if (str == null)
 					break;
-
-				inTest.append(str);
+				if (str.length()!=0) {
+					inTest.append(str);
+					inTest.append("\n");
+				}
+				
 			}
 		} catch (IOException e) {
 			System.out
@@ -70,12 +105,26 @@ public class Parser {
 		if (!openInputFile()) {
 			System.exit(1);
 		}
+		
+//		System.out.println("Input:");
+//		System.out.println(inTest.toString());
+		
+		String string = inTest.toString();
+		System.out.println(string.trim());
+		
+		Pattern p = Pattern.compile("\\p{L}*");
+
+		Matcher m = p.matcher(string);
+		
+		System.out.println(m.matches());
+		
+		
 
 	}
 
 	public static void main(String[] args) {
 
-		Parser parser = new Parser();
+		Parser parser = new Parser("input2.txt", "");
 
 		parser.parse();
 
